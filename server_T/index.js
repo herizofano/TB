1dafd422244bff4af923bad3872fa9cc86be8282
herizo fanomzana <herizo.ratefiarison@gmail.com>
2023-01-30 20:34:19 +0300

    server final

--- server_T/index.js
+++ server_T/index.js
@@ -1,43 +1,46 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-
-import clientRoutes from './routes/clients.js';
-import generalRoutes from './routes/generals.js';
-import managementRoutes from './routes/managements.js';
-import campagnesRoutes from './routes/campagnes.js';
-import router from './routes/route';
-
+import clientRoutes from "./routes/clients.js";
+import generalRoutes from "./routes/generals.js";
+import managementRoutes from "./routes/managements.js";
+import campagnesRoutes from "./routes/campagnes.js";
+import router from "./routes/route.js";
 
 //data import
 
-import User from './models/User.js';
-import Product from './models/Product.js';
-import ProductStat from './models/ProductStat.js';
-import Transaction from './models/Transaction.js';
-import OverallStat from './models/OverallStat.js';
-import AffiliateStat from './models/AffiliateStat.js';
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
 
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from './datas/index.js'
-
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./datas/index.js";
 
 /** CONFIGURATION */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: 'cross-origin'}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-
 
 /** ROUTES */
 
@@ -47,16 +50,16 @@ app.use("/management", managementRoutes);
 app.use("/sales", campagnesRoutes);
 app.use("/api", router);
 
-
 /** MONGOOSE SETUP */
 
 const PORT = process.env.PORT || 9000;
 
-mongoose.connect(process.env.MONGO_URL,{
-
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /*ONLY ADD DATA ONE TIME*/
@@ -66,4 +69,5 @@ mongoose.connect(process.env.MONGO_URL,{
     //OverallStat.insertMany(dataOverallStat);
     //User.insertMany(dataUser);
     //AffiliateStat.insertMany(dataAffiliateStat);
-}).catch((err) => console.log(`${err} Server not connected`));
+  })
+  .catch((err) => console.log(`${err} Server not connected`));
--- server_T/models/User.model.js
+++ server_T/models/User.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const UserSchema = new mongoose.Schema({
+export const UseSchema = new mongoose.Schema({
     username : {
         type: String,
         required : [true, "Please provide unique Username"],
@@ -23,4 +23,4 @@ export const UserSchema = new mongoose.Schema({
     profile: { type: String}
 });
 
-export default mongoose.model.Users || mongoose.model('User', UserSchema);
+export default mongoose.model.Users || mongoose.model('Use', UseSchema);
